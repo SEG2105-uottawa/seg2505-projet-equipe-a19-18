e59ddb4286291417745e90cf23a0fbafb64596f2
Littlehadi <helsa074@uottawa.ca>
2019-10-17 13:41:17 -0400

    Created a new version for apk

---
+++ SEG_Projet4/app/src/main/java/com/example/momo_willie/seg_projet4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.momo_willie.seg_projet4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

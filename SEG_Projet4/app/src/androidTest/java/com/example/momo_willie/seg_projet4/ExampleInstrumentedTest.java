0eb869b8ae361ce3bda63612e02c7a71b1f00bf0
Littlehadi <helsa074@uottawa.ca>
2019-10-17 13:45:41 -0400

    Modified Apk login screen

--- SEG_Projet4/app/src/androidTest/java/com/example/momo_willie/seg_projet4/ExampleInstrumentedTest.java
+++ SEG_Projet4/app/src/androidTest/java/com/example/momo_willie/seg_projet4/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.momo_willie.seg_projet4;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- SEG_Projet4/app/src/main/java/com/example/momo_willie/seg_projet4/MainActivity.java
+++ SEG_Projet4/app/src/main/java/com/example/momo_willie/seg_projet4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.momo_willie.seg_projet4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {

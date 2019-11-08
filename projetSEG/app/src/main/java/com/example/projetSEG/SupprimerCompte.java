df6c10b56b1fa0c897df80e164cfcc57c392de05
Simon Paquette <spaqu044@uottawa.ca>
2019-11-08 14:21:14 -0500

    layout

--- projetSEG/app/src/main/java/com/example/projetSEG/SupprimerCompte.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/SupprimerCompte.java
@@ -1,10 +1,11 @@
 package com.example.projetSEG;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SupprimerCompte  extends AppCompatActivity {
+import android.os.Bundle;
+
+public class SupprimerCompte extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

667f2c9efa7013383b9d91a6ea6faf9e2a8325c4
Simon Paquette <spaqu044@uottawa.ca>
2019-11-19 13:42:04 -0500

    employe

--- projetSEG/app/src/main/java/com/example/projetSEG/Employe.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/Employe.java
@@ -44,18 +44,28 @@ public class Employe extends AppCompatActivity {
         accept.setVisibility(View.VISIBLE);
         change.setVisibility(View.GONE);
 
+        String adresse = viewAdresse.getText().toString();
+        editAdresse.setText(adresse);
         editAdresse.setVisibility(View.VISIBLE);
         viewAdresse.setVisibility(View.GONE);
 
+        String tel = viewTel.getText().toString();
+        editTel.setText(tel);
         editTel.setVisibility(View.VISIBLE);
         viewTel.setVisibility(View.GONE);
 
+        String nom = viewNom.getText().toString();
+        editNom.setText(nom);
         editNom.setVisibility(View.VISIBLE);
         viewNom.setVisibility(View.GONE);
 
+        String assurance = viewAssurance.getText().toString();
+        editAssurance.setText(assurance);
         editAssurance.setVisibility(View.VISIBLE);
         viewAssurance.setVisibility(View.GONE);
 
+        String paiment = viewPaiment.getText().toString();
+        editPaiment.setText(paiment);
         editPaiment.setVisibility(View.VISIBLE);
         viewPaiment.setVisibility(View.GONE);
     }
@@ -80,18 +90,28 @@ public class Employe extends AppCompatActivity {
         accept.setVisibility(View.GONE);
         change.setVisibility(View.VISIBLE);
 
+        String adresse = editAdresse.getText().toString();
+        viewAdresse.setText(adresse);
         editAdresse.setVisibility(View.GONE);
         viewAdresse.setVisibility(View.VISIBLE);
 
+        String tel = editTel.getText().toString();
+        viewTel.setText(tel);
         editTel.setVisibility(View.GONE);
         viewTel.setVisibility(View.VISIBLE);
 
+        String nom = editNom.getText().toString();
+        viewNom.setText(nom);
         editNom.setVisibility(View.GONE);
         viewNom.setVisibility(View.VISIBLE);
 
+        String assurance = editAssurance.getText().toString();
+        viewAssurance.setText(assurance);
         editAssurance.setVisibility(View.GONE);
         viewAssurance.setVisibility(View.VISIBLE);
 
+        String paiment = editPaiment.getText().toString();
+        viewPaiment.setText(paiment);
         editPaiment.setVisibility(View.GONE);
         viewPaiment.setVisibility(View.VISIBLE);
     }

9a13fba4817ed6141ee98d0ddcda54231d80b75e
Simon Paquette <spaqu044@uottawa.ca>
2019-11-19 18:57:01 -0500

    liste service

--- projetSEG/app/src/main/java/com/example/projetSEG/ListeServices.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/ListeServices.java
@@ -6,23 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ListeServices extends AppCompatActivity {
+
+    Button modifier, supprimer, ajouter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_services);
 
-        Button modifier = findViewById(R.id.btnModifier);
-        Button supprimer = findViewById(R.id.btnSupprimer);
-        Button ajouter = findViewById(R.id.btnAjouter);
+        modifier = findViewById(R.id.btnModifier);
+        supprimer = findViewById(R.id.btnSupprimer);
+        ajouter = findViewById(R.id.btnAjouter);
+
+
         Intent intent = getIntent();
         String fromEmploye = intent.getStringExtra("fromEmploye");
-        if(fromEmploye == "true") {
+
+        if(fromEmploye == null) {
+            modifier.setVisibility(View.VISIBLE);
+            supprimer.setVisibility(View.VISIBLE);
+            ajouter.setVisibility(View.GONE);
+        } else if (fromEmploye.equals("true")) {
             modifier.setVisibility(View.GONE);
             supprimer.setVisibility(View.GONE);
             ajouter.setVisibility(View.VISIBLE);
         }
+
+
+
+
     }
 }

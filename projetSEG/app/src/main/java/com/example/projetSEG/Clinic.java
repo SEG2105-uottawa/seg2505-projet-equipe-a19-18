4b014260b20b789d5ffe20e883979412c40a056c
Simon Paquette <spaqu044@uottawa.ca>
2019-11-20 18:17:46 -0500

    sauvegarde clinique database

--- projetSEG/app/src/main/java/com/example/projetSEG/Clinic.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/Clinic.java
@@ -2,10 +2,10 @@ package com.example.projetSEG;
 
 public class Clinic {
 
-    public String adresse, nom, assurance, paiment;
-    public int telephone;
+    public String adresse, telephone, nom, assurance, paiment;
 
-    public Clinic (String adresse, int telephone, String nom, String assurance, String paiment) {
+
+    public Clinic (String adresse, String telephone, String nom, String assurance, String paiment) {
         this.adresse = adresse;
         this.telephone = telephone;
         this.nom = nom;
--- projetSEG/app/src/main/java/com/example/projetSEG/Employe.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/Employe.java
@@ -1,5 +1,6 @@
 package com.example.projetSEG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,9 +11,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class Employe extends AppCompatActivity {
 
+    DatabaseReference dataEmploye;
+    HashMap<String,String> list;
 
+    String nameOfUser;
+
+    String key, username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +39,38 @@ public class Employe extends AppCompatActivity {
 
         TextView title = findViewById(R.id.textViewEmploye);
         Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        title.setText("Bienvenue " +name+ "! Vous êtes connecté en tant qu'employé");
+        nameOfUser = intent.getStringExtra("name");
+        title.setText("Bienvenue " +nameOfUser+ "! Vous êtes connecté en tant qu'employé");
+
+
+        dataEmploye = FirebaseDatabase.getInstance().getReference("Employe");
+        list = new HashMap<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dataEmploye.addValueEventListener((new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Object infoRaw = postSnapshot.getValue();
+                    key = postSnapshot.getKey();
+                    HashMap info = (HashMap) infoRaw;
+                    username = (String) info.get("username");
+
+
+                    list.put(key,username);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        }));
     }
 
     public void change(View view) {
@@ -137,6 +185,19 @@ public class Employe extends AppCompatActivity {
             viewPaiment.setText(paiment);
             editPaiment.setVisibility(View.GONE);
             viewPaiment.setVisibility(View.VISIBLE);
+
+
+            Set set = list.entrySet();
+            Iterator iterator = set.iterator();
+            while(iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry)iterator.next();
+                if (nameOfUser.equals(entry.getValue().toString())) {
+                    Clinic clinic = new Clinic(adresse, tel, nom, assurance, paiment);
+                    dataEmploye.child(entry.getKey().toString()).child("clinique").setValue(clinic);
+                }
+            }
+
+
         }
     }
 

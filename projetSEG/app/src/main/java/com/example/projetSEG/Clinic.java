ac2613be93948f1f08b76aca67b274a106fbe50c
Littlehadi <helsa074@uottawa.ca>
2019-11-20 23:04:24 -0500

    Update Clinic.java

--- projetSEG/app/src/main/java/com/example/projetSEG/Clinic.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/Clinic.java
@@ -5,11 +5,33 @@ public class Clinic {
 
     public String adresse, telephone, nom, assurance, paiment;
 
+    public Clinic(){}
+
     public Clinic (String adresse, String telephone, String nom, String assurance, String paiment) {
         this.adresse = adresse;
         this.telephone = telephone;
         this.nom = nom;
         this.assurance = assurance;
         this.paiment = paiment;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAssurance() {
+        return assurance;
+    }
+
+    public String getPaiment() {
+        return paiment;
     }
 }

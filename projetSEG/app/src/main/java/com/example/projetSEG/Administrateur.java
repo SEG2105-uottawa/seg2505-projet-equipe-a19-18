d0d7f9789390708b0ce3777bd284b601d88cca8b
Littlehadi <helsa074@uottawa.ca>
2019-11-20 22:35:12 -0500

    Updates Files

--- projetSEG/app/src/main/java/com/example/projetSEG/Administrateur.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/Administrateur.java
@@ -1,24 +1,49 @@
 package com.example.projetSEG;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //PAGE POUR UN ADMIN
 public class Administrateur extends AppCompatActivity {
+
+    ListView listViewUsers;
+    List<User> users = new ArrayList<>();
+
+    private DatabaseReference data = FirebaseDatabase.getInstance().getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrateur);
 
+        listViewUsers = (ListView) findViewById(R.id.list);
+
+        data = FirebaseDatabase.getInstance().getReference("users");
+
         TextView action = findViewById(R.id.action);
         Intent intent = getIntent();
         String actionName = intent.getStringExtra("nouveauService");
         action.setText(actionName);
+    }
+
+    private boolean deleteUser(String id) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(id);
+        dR.removeValue();
+        Toast.makeText(getApplicationContext(), "User Deleted", Toast.LENGTH_LONG).show();
+        return true;
     }
 
     public void nouveauService(View view){
--- projetSEG/app/src/main/java/com/example/projetSEG/SupprimerCompte.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/SupprimerCompte.java
@@ -1,17 +1,41 @@
 package com.example.projetSEG;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //PAGE POUR SUPPRIMER UN COMPTE PATIENT OU EMPLOYE PAR UN ADMIN
 public class SupprimerCompte extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_supprimer_compte);
+
+        ListView listViewUsers;
+        List<User> users = new ArrayList<>();
+
+            private DatabaseReference data = FirebaseDatabase.getInstance().getReference();
 
 
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_supprimer_compte);
+
+                listViewUsers = (ListView) findViewById(R.id.list);
+
+                data = FirebaseDatabase.getInstance().getReference("users");
+            }
+
+        private boolean deleteUser(String id) {
+            DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(id);
+            dR.removeValue();
+            Toast.makeText(getApplicationContext(), "User Deleted", Toast.LENGTH_LONG).show();
+            return true;
+        }
     }
-}

ddbe6dffc714048d5a943f07752f392da471cf97
Simon Paquette <spaqu044@uottawa.ca>
2019-11-20 22:31:51 -0500

    Update ClinicService.java

--- projetSEG/app/src/main/java/com/example/projetSEG/ClinicService.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/ClinicService.java
@@ -1,15 +1,40 @@
 package com.example.projetSEG;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 //PAGE POUR PRESENTER LES SERVICE OFFERT DE LA CLINIQUE
 public class ClinicService extends AppCompatActivity {
 
     String id;
+
+    DatabaseReference dataEmploye;
+    DatabaseReference data;
+
+    ListView list;
+    ArrayList<String> array = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    int selectedItem = -1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +43,46 @@ public class ClinicService extends AppCompatActivity {
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
+
+        dataEmploye = FirebaseDatabase.getInstance().getReference("Employe");
+
+
+        list = findViewById(R.id.listeOffert);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,array);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem = position;
+            }
+        });
+
+
+        data = dataEmploye.child(id).child("clinique").child("service");
+
+        data.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                String value = dataSnapshot.getValue(ServiceObject.class).toString();
+                array.add(value);
+                adapter.notifyDataSetChanged();
+
+
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
 
 

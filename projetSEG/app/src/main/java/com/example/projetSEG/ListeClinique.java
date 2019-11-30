2a740351a7631aaeb19b68c8f60f46c61857cefa
Simon Paquette <spaqu044@uottawa.ca>
2019-11-30 15:50:59 -0500

    recherche terminer (cependant il faut ecricre le string exact)

--- projetSEG/app/src/main/java/com/example/projetSEG/ListeClinique.java
+++ projetSEG/app/src/main/java/com/example/projetSEG/ListeClinique.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -37,13 +38,14 @@ public class ListeClinique extends AppCompatActivity {
     String clinicName;
 
     String rAdresse, rHeure, rService;
+    TextView recherche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_clinique);
 
-
+        recherche  = findViewById(R.id.recherche);
         Intent intent = getIntent();
         rAdresse = intent.getStringExtra("adresse");
         rHeure = intent.getStringExtra("heure");
@@ -73,37 +75,58 @@ public class ListeClinique extends AppCompatActivity {
                     String clinicName = (String) clinic.get("nom");
 
                     String clinicAdress = (String) clinic.get("adresse");
-                    HashMap clinicHour = (HashMap) clinic.get("clinicHour");
+                    HashMap<String, String> clinicHour = (HashMap) clinic.get("clinicHour");
                     HashMap<String, HashMap> idService = (HashMap) clinic.get("service");
                     ArrayList services = new ArrayList();
+                    ArrayList heures = new ArrayList();
 
-                    for(Map.Entry<String, HashMap> entry : idService.entrySet()) {
-                        String k = entry.getKey();
-                        HashMap v = entry.getValue();
-                        services.add(v.get("id"));
+                    if (idService!=null) {
+                        for(Map.Entry<String, HashMap> entry : idService.entrySet()) {
+                            HashMap v = entry.getValue();
+                            services.add(v.get("id"));
+                        }
                     }
+                    if (clinicHour!=null) {
+                        for(Map.Entry<String, String> entry : clinicHour.entrySet()) {
+                            heures.add(entry.getValue());
+                        }
+                    }
+
 
 
                     String value = clinicName;
 
                     //rechercher par adresse
                     if (rAdresse!=null) {
+                        recherche.setText("Recherche par adresse");
                         if (rAdresse.equals(clinicAdress)) {
                             keyList.add(dataSnapshot.getKey());
                             array.add(value);
                         }
+                        if (array.isEmpty()) {
+                            recherche.setText("Recherche par adresse : AUCUN RÉSULTAT TROUVÉ");
+                        }
 
                     //recherche par heure
                     } else if (rHeure!=null) {
+                        recherche.setText("Recherche par heure");
 
+                        for (int i = 0; i < heures.size(); i++) {
 
-                        if (true) {
-                            keyList.add(dataSnapshot.getKey());
-                            array.add(value);
+                            if (rHeure.equals(heures.get(i))) {
+                                keyList.add(dataSnapshot.getKey());
+                                array.add(value);
+                                break;
+                            }
+                        }
+
+                        if (array.isEmpty()) {
+                            recherche.setText("Recherche par heure : AUCUN RÉSULTAT TROUVÉ");
                         }
 
                     //recherche par service
                     } else if (rService!=null) {
+                        recherche.setText("Recherche par service");
 
                         for (int i = 0; i < services.size(); i++) {
 
@@ -112,6 +135,10 @@ public class ListeClinique extends AppCompatActivity {
                                 array.add(value);
                                 break;
                             }
+                        }
+
+                        if (array.isEmpty()) {
+                            recherche.setText("Recherche par service : AUCUN RÉSULTAT TROUVÉ");
                         }
 
 

f0955a131448b98e0eaf62e5b88a77405271f5dd
Simon Paquette <spaqu044@uottawa.ca>
2019-12-01 11:43:36 -0500

    config

---
+++ projetSEG/app/src/test/java/com/example/projetSEG/Livrable4.java
@@ -0,0 +1,17 @@
+package com.example.projetSEG;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.projetSEG.Livrable4;
+
+public class Livrable4 {
+
+    @Test
+    public void test1() {
+        int i = 0;
+        assertEquals("Conversion from celsius to fahrenheit failed", 0, i, 0.001);
+    }
+}

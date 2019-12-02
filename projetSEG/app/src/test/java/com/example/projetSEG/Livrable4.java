448538365a1706e21120b8ac0b1736d057432d89
Simon Paquette <spaqu044@uottawa.ca>
2019-12-01 20:36:01 -0500

    unit1

--- projetSEG/app/src/test/java/com/example/projetSEG/Livrable4.java
+++ projetSEG/app/src/test/java/com/example/projetSEG/Livrable4.java
@@ -4,14 +4,29 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.example.projetSEG.Livrable4;
 
 public class Livrable4 {
 
     @Test
-    public void test1() {
-        int i = 0;
-        assertEquals("test1", 0, i, 0.001);
+    public void testRatingObject() {
+        Rating rating;
+        rating = new Rating(4.5f, "commantaire");
+        float rate;
+        rate = rating.star;
+
+        assertEquals("testRatingObject", 4.5f, rate, 0.001);
+    }
+
+    @Test
+    public void test() {
+
     }
 }
